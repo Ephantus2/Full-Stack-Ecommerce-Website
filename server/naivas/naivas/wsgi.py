bd0baffb9cd311cd02d821e9730a61bd97bb8d5c
Ephantus2 <ephantusmwago6@gmail.com>
2025-11-08 11:19:17 +0300

    auto create superuser

--- server/naivas/naivas/wsgi.py
+++ server/naivas/naivas/wsgi.py
@@ -14,3 +14,11 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naivas.settings')
 
 application = get_wsgi_application()
+
+# 👇 Add this after 'application' is defined
+try:
+    from accounts.create_superuser import create_superuser
+    create_superuser()
+except Exception as e:
+    print(f"Superuser creation failed: {e}")
+
